2dfbe86b68a06fee80ae0a901b9144533855caa9
saindon <71152907+saindon@users.noreply.github.com>
2023-04-26 01:25:18 -0400

    Created 2 database objects
    Created the tables 'User' and 'UserType'. Will put more work into this tomorrow

--- team2lms/catalog/models.py
+++ team2lms/catalog/models.py
@@ -1,4 +1,26 @@
 from django.db import models
 
 # Create your models here.
-# git test
+
+class User(models.Model):
+
+    #fields
+    UserID = models.CharField(MaxLength = 6, primary_key = True)
+    Password = models.CharField(MaxLength = 100)
+    UserFirstName = models.CharField(MaxLength = 20)
+    UserLastName = models.CharField(MaxLength = 20)
+    PhoneNumber = models.CharField(MaxLength = 6)
+    Email = models.EmailField()
+    AddressLine1 = models.CharField(MaxLength = 50)
+    AddressLine2 = models.CharField(MaxLength = 50)
+    City = models.CharField(MaxLength = 50)
+    State = models.CharField(MaxLength = 20)
+    ZipCode = models.CharField(MaxLength = 5)
+    UserTypeID = models.ForeignKey('UserType.UserTypeID')
+    NumBooksLost = models.IntegerField(MaxLength = 6, default=0)
+    TotalFeesDue = models.IntegerField(MaxLength = 6, default = 0)
+
+
+class UserType(models.Model):
+    UserTypeID = models.CharField(MaxLength = 5, primary_key=True)
+    UserType = models.CharField()
